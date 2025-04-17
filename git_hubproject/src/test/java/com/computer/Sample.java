f231d815cfa66f2ea44699e20c628880f2b7aaf7
sneha <sneha@LAPTOP-SB7RE8KB>
2025-04-17 11:23:58 +0530

    change made

---
+++ git_hubproject/src/test/java/com/computer/Sample.java
@@ -0,0 +1,22 @@
+package com.computer;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample 
+{
+	@Test
+public void milege() {
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	driver.get("https://www.bajajmall.in/");
+	Reporter.log("Deo milege is 60", true);
+	Reporter.log("Deo milege is 60", true);
+	Reporter.log("Deo milege is 60", true);
+}
+}
